c09ff7d3a7e8caf40553f2ed6593809fb9754a09
Erol Emre Güleç <emreglcfb@gmail.com>
2021-08-26 15:46:35 +0300

    chakra ui added

--- client/src/App.js
+++ client/src/App.js
@@ -7,24 +7,27 @@ import {
 } from "react-router-dom";
 import './App.css';
 import Navbar from "./components/Navbar";
+import Signin from"./pages/Auth/Signin";
+import Signup from "./pages/Auth/Signup";
 
 function App() {
   return (
     <Router>
       <div>
-
-
         <Navbar />
-        <Switch>
-          <Route path="/" component={About} />
-
-        </Switch>
+        <div id="content">
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/signin" component={Signin} />
+            <Route path="/signup" component={Signup} />
+          </Switch>
+        </div>
       </div>
     </Router>
   );
 }
 
-function About() {
+function Home() {
   return <h2>About</h2>
 }
 export default App;
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,15 +1,29 @@
 import React from 'react'
-import{Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import styles from "./styles.module.css"
+import { Button, ButtonGroup } from "@chakra-ui/react"
 function Navbar() {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-            </ul>
+        <nav className={styles.nav}>
+            <div className={styles.left}>
+                <div className="logo">
+                    <Link to="/">e-commerce</Link>
+                </div>
+                <ul className={styles.menu}>
+                    <li>
+                        <Link to="/">Products</Link>
+                        
+                    </li>
+                </ul>
+            </div>
+            <div className={styles.right}>
+            <Link to="/signin"><Button colorScheme="red">Login</Button></Link>
+                
+            <Link to="/signup"><Button colorScheme="red">Register</Button></Link>
+
+            </div>
         </nav>
     )
 }
 
-export default Navbar
+export default Navbar;
